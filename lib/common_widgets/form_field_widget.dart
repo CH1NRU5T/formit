eadef339d6191250e67fc581b1b090023baebe4d
ch1nru5t <chinrust@gmail.com>
2023-09-27 23:14:26 +0530

    drop down added

--- lib/common_widgets/form_field_widget.dart
+++ lib/common_widgets/form_field_widget.dart
@@ -13,6 +13,41 @@ class FormFieldWidget extends StatefulWidget {
 }
 
 class _FormFieldWidgetState extends State<FormFieldWidget> {
+  final List<DropdownMenuItem> _items = [
+    const DropdownMenuItem(
+      value: FormFieldType.shortAnswer,
+      child: Text('Short Answer'),
+    ),
+    const DropdownMenuItem(
+      value: FormFieldType.paragraph,
+      child: Text('Paragraph'),
+    ),
+    const DropdownMenuItem(
+      value: FormFieldType.multipleChoice,
+      child: Text('Multiple Choice'),
+    ),
+    const DropdownMenuItem(
+      value: FormFieldType.checkBoxes,
+      child: Text('Checkboxes'),
+    ),
+    const DropdownMenuItem(
+      value: FormFieldType.dropDown,
+      child: Text('Dropdown'),
+    ),
+    const DropdownMenuItem(
+      value: FormFieldType.fileUpload,
+      child: Text('File Upload'),
+    ),
+    const DropdownMenuItem(
+      value: FormFieldType.date,
+      child: Text('Date'),
+    ),
+    const DropdownMenuItem(
+      value: FormFieldType.time,
+      child: Text('Time'),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -33,19 +68,28 @@ class _FormFieldWidgetState extends State<FormFieldWidget> {
                   },
                 ),
               ),
-              // DropdownButton(items: items, onChanged: onChanged)
+              DropdownButton(
+                value: widget.field.type!,
+                items: _items,
+                onChanged: (value) {
+                  context.read<FormProvider>().changeField(
+                        widget.index,
+                        type: value as FormFieldType,
+                      );
+                },
+              ),
             ],
           ),
           const SizedBox(height: 10),
           switch (widget.field.type!) {
-            FormFieldType.shortAnswer => const SizedBox(),
-            FormFieldType.paragraph => const SizedBox(),
-            FormFieldType.multipleChoice => const SizedBox(),
-            FormFieldType.checkBoxes => const SizedBox(),
-            FormFieldType.dropDown => const SizedBox(),
-            FormFieldType.fileUpload => const SizedBox(),
-            FormFieldType.date => const SizedBox(),
-            FormFieldType.time => const SizedBox(),
+            FormFieldType.shortAnswer => Text(widget.field.type.toString()),
+            FormFieldType.paragraph => Text(widget.field.type.toString()),
+            FormFieldType.multipleChoice => Text(widget.field.type.toString()),
+            FormFieldType.checkBoxes => Text(widget.field.type.toString()),
+            FormFieldType.dropDown => Text(widget.field.type.toString()),
+            FormFieldType.fileUpload => Text(widget.field.type.toString()),
+            FormFieldType.date => Text(widget.field.type.toString()),
+            FormFieldType.time => Text(widget.field.type.toString()),
           }
           // TextFormField(
           //   decoration: const InputDecoration(
